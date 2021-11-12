3d983d628e335254119f4ffec9769b20144204ab
Crowdbotics <team@crowdbotics.com>
2021-11-12 20:11:58 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/ideapros_king_of_cr_31945/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ideapros_king_of_cr_31945;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "ideapros_king_of_cr_31945";
+  }
+}
